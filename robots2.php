82b3adcc4926f09bdac84bf972a7f7472239588b
ElmayorG <47934298+ElmayorG@users.noreply.github.com>
2019-02-25 01:42:00 -0600

    Delete robots2.php

--- robots2.php
+++
@@ -1,8 +0,0 @@
-<?Php
-for ($i = 0; $i <= 1; $i++){
-$file = file('https://www.indeed.com.mx/jobs?q=medio+tiempo&sort=date&start='.$i*10);
-    foreach($file as $linenum => $line){
-    	echo "<b>Line #{$linenum}</b> ".htmlspecialchars($line).'</br>';
-	}
-}
-?>
